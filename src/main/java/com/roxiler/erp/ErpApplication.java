78ff2439a745f551a9aad8b5b1fecb2679c930ad
Himanshu-Roxiler <himanshu.sahani@roxiler.com>
2023-08-09 11:30:11 +0530

    enable auditing for base entity

--- src/main/java/com/roxiler/erp/ErpApplication.java
+++ src/main/java/com/roxiler/erp/ErpApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
 @EnableJpaRepositories("com.roxiler.erp.repository")
 @EntityScan("com.roxiler.erp.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class ErpApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/roxiler/erp/model/BaseEntity.java
+++ src/main/java/com/roxiler/erp/model/BaseEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
