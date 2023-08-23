dfed579384a1588b2d6aca9d062960d6276e8de4
Himanshu-Roxiler <himanshu.sahani@roxiler.com>
2023-08-23 15:18:34 +0530

    change order to -100

--- src/main/java/com/roxiler/erp/config/WebConfig.java
+++ src/main/java/com/roxiler/erp/config/WebConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class WebConfig {
 
     private static final Long MAX_AGE = 3600L;
-    private static final int CORS_FILTER_ORDER = -102;
+    private static final int CORS_FILTER_ORDER = -100;
 
     @Bean
     public FilterRegistrationBean corsFilter() {
