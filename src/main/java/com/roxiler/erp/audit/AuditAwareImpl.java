adcafff86f6981e2a0f5c5ba57161069596c1b18
Himanshu-Roxiler <himanshu.sahani@roxiler.com>
2023-09-04 11:45:42 +0530

    fix created by response

--- src/main/java/com/roxiler/erp/audit/AuditAwareImpl.java
+++ src/main/java/com/roxiler/erp/audit/AuditAwareImpl.java
@@ -1,6 +1,11 @@
 package com.roxiler.erp.audit;
 
+import com.roxiler.erp.dto.auth.UserDto;
+import com.roxiler.erp.model.Users;
+import com.roxiler.erp.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +14,18 @@ import java.util.Optional;
 @Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
 
+    @Autowired
+    private UsersRepository usersRepository;
+
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.getPrincipal().getClass() == String.class) {
+            return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
+        UserDto userDto = (UserDto) auth.getPrincipal();
+        Optional<Users> user = usersRepository.findById(userDto.getId());
+        return user.map(users -> users.getFirstName() + " " + users.getLastName());
+
     }
 }
