371a380835c8c7453ef555dda334c8ac984b7c82
sunil18Roxiler <sunil.otale@roxiler.com>
2023-09-11 17:42:51 +0530

    dto changes

--- src/main/java/com/roxiler/erp/dto/holiday/UpdateHolidayDto.java
+++ src/main/java/com/roxiler/erp/dto/holiday/UpdateHolidayDto.java
@@ -3,6 +3,7 @@ package com.roxiler.erp.dto.holiday;
 import java.util.Date;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -17,7 +18,6 @@ public class UpdateHolidayDto {
     @Size(min=3, message = "Length should not be less than 3")
     private String holidayDescription;
 
-    @NotBlank(message = "Holiday Date should not be blank")
     private Date holidayDate;
 
 }
